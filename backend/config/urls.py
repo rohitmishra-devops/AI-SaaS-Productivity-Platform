5f1cee226a69d6b2b74acc9915ceee523903aa09
Rohit Mishra <rohitmishra13214@gmail.com>
2026-08-08 14:29:47 +0530

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+     path("api/auth/", include("apps.users.urls")),
+    path("api/tasks/", include("apps.tasks.urls")),
 ]
